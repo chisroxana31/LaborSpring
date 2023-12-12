3a86f756a4484da12b5f88fec685321d2a98113d
chisroxana31 <chisroxana31@yahoo.com>
2023-12-12 14:24:09 +0200

    Third commit labor with Springboot and Rest framework.

---
+++ src/main/java/Controller/GenreController.java
@@ -0,0 +1,52 @@
+package Controller;
+
+import Domain.Genre;
+import Repo.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/genres")
+public class GenreController {
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @GetMapping
+    public List<Genre> getAllGenres() {
+        return genreRepository.findAll();
+    }
+
+    @GetMapping("/{genreId}")
+    public Genre getGenreById(@PathVariable Long genreId) {
+        return genreRepository.findById(genreId).orElse(null);
+    }
+
+    @PostMapping
+    public void addGenre(@RequestBody Genre genre) {
+        genreRepository.save(genre);
+    }
+
+    @PutMapping("/{genreId}")
+    public void updateGenre(@PathVariable Long genreId, @RequestBody Genre updatedGenre) {
+        Genre existingGenre = genreRepository.findById(genreId).orElse(null);
+        if (existingGenre != null) {
+            existingGenre.setName(updatedGenre.getName());
+            genreRepository.save(existingGenre);
+        }
+    }
+
+    @GetMapping("/findByName/{name}")
+    public List<Genre> getGenresByName(@PathVariable String name) {
+        return genreRepository.findByName(name);
+    }
+
+    // Add other CRUD operations and business logic...
+
+    @DeleteMapping("/{genreId}")
+    public void deleteGenre(@PathVariable Long genreId) {
+        genreRepository.deleteById(genreId);
+    }
+}
---
+++ src/main/java/Repo/GenreRepository.java
@@ -0,0 +1,13 @@
+package Repo;
+
+import Domain.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+
+    List<Genre> findByName(String name);
+}
